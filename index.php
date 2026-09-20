7f9b9ff957e115152a354d13c2fd52ad0dda836f
mcguffin <joern@flyingletters.com>
2026-09-20 18:42:02 +0200

    Release 0.0.1

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Enable Infinite scroll for Post Template Blocks inside a query loop
  * Author: mcguffin
  * Author URI: https://github.com/mcguffin
- * Version: 0.0.0
+ * Version: 0.0.1
  * Requires PHP: 7.4
  * Text Domain: infinite-scroll
  * License: GPL2+
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: #### donate link ####
 Requires at least: 6.0
 Requires PHP: 8.2
 Tested up to: 7.1
-Stable tag: 0.0.0
+Stable tag: 0.0.1
 License: GPLv3 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
